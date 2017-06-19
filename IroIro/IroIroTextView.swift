57cb9127dd7df1608ececd70560e3307f373cd0a
SilverEzhik <i@ezhik.me>
2017-06-19 06:25:14 -0700

    Just a few tweaks short of perfection

---
+++ IroIro/IroIroTextView.swift
@@ -0,0 +1,21 @@
+//
+//  IroIroTextView.swift
+//  IroIro
+//
+//  Created by Ezhik on 6/19/17.
+//  Copyright © 2017 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class IroIroTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
